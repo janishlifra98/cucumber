f51bc92cbab2d4509919ddbfae173cf3d6100d79
janishlifra98 <52442063+janishlifra98@users.noreply.github.com>
2019-07-02 16:22:06 +0530

    Update run.java

--- src/test/java/Runner/run.java
+++ src/test/java/Runner/run.java
@@ -6,7 +6,7 @@ import cucumber.api.CucumberOptions;
 import cucumber.api.junit.Cucumber;
 @RunWith(Cucumber.class)
 //@CucumberOptions(features={"src/main/resources/eg1.feature"},glue="stepdefinition",plugin="html:target/htmlreport")
-@CucumberOptions(features={"src/main/resources/eg4.feature"},glue="stepdefinition",plugin="json:target/cucumber.json")
+@CucumberOptions(features={"src/main/resources/eg4.feature"},glue="stepdefinition",plugin="json:target/jsonreport.json")
 //@CucumberOptions(features={"src/main/resources/eg4.feature"},glue="stepdefinition",plugin="junit:target/xmlreport.xml")
 //tags={"@regression"})
 //tags={"@smoke","@regression"})
